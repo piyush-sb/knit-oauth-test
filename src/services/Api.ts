692b1f4a8b0de2ea6249d51b1c62da072d7711a3
piyush-sb <piyush@superbeings.ai>
2023-01-24 13:18:13 +0530

    fix : Update api baseUrl

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const prodApi: AxiosInstance = axios.create({
-  baseURL: `https://4582-2409-4070-2b9a-770f-ccc5-70c3-e563-8dd3.in.ngrok.io/`,
+  baseURL: `https://frontend-engine.sandbox.getknit.dev/`,
   headers: {
     "ngrok-skip-browser-warning": "true",
     Accept: "application/json",
@@ -10,7 +10,7 @@ const prodApi: AxiosInstance = axios.create({
 });
 
 const sanboxApi: AxiosInstance = axios.create({
-  baseURL: `https://4582-2409-4070-2b9a-770f-ccc5-70c3-e563-8dd3.in.ngrok.io/`,
+  baseURL: `https://frontend-engine.sandbox.getknit.dev/`,
   headers: {
     "ngrok-skip-browser-warning": "true",
     Accept: "application/json",
