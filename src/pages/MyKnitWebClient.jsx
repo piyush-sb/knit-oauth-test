acb8cb5726e60230ca3b92236a64b3d9d314a570
NischalChenna <chennanischal5000@gmail.com>
2023-02-27 16:41:59 +0530

    getting apiKey from mocky

--- src/pages/MyKnitWebClient.jsx
+++ src/pages/MyKnitWebClient.jsx
@@ -2,24 +2,49 @@ import { KnitWeb } from "../components/KnitWeb";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [sesssionToken, setSessionToken] = useState('');
+  const [sesssionToken, setSessionToken] = useState("");
   const [integrationId, setIntegrationId] = useState(null);
+  const [apiKey, setApiKey] = useState(
+    "09e8b0adebb2df4bfc205be8ffe9e3c24297c37d4e005286b2f3bab773e5e24f"
+  );
 
   useEffect(() => {
-   newSessionFn()
+    newSessionFn();
   }, []);
 
-  const  newSessionFn = (e) =>{
-e?.preventDefault();
-fetch('https://mock-knit-session.onrender.com/getSessionToken', {method:"GET"}).then(res=> res.json()).then(r=>{
-    setSessionToken(r.msg.token);
-})
-
-
-}
+  const newSessionFn = (e) => {
+    e?.preventDefault();
+    fetch(`https://run.mocky.io/v3/e1c80e42-1038-40fe-b832-97889ab160d2`, {
+      method: "GET",
+      headers: {
+        "ngrok-skip-browser-warning": true,
+      },
+    })
+      .then((res) => res.json())
+      .then((r) => {
+        setApiKey(r.msg.apiKey);
+      });
+    fetch("https://api.sandbox.getknit.dev/v1.0/auth.createSession", {
+      method: "POST",
+      headers: {
+        "ngrok-skip-browser-warning": true,
+        Authorization: "Bearer " + apiKey,
+      },
+      body: JSON.stringify({
+        originOrgId: "Rapido",
+        originOrgName: "Rapido",
+        originUserEmail: "nischal@rapido.ai",
+        originUserName: "Nischal Chenna",
+      }),
+    })
+      .then((res) => res.json())
+      .then((r) => {
+        setSessionToken(r.msg.token);
+      });
+  };
   const onSuccessFn = (e) => {
     e.preventDefault();
-    setInetgrationId(e.detail['integration-id']);
+    setInetgrationId(e.detail["integration-id"]);
   };
 
   return (
@@ -30,7 +55,7 @@ fetch('https://mock-knit-session.onrender.com/getSessionToken', {method:"GET"}).
       onSuccess={onSuccessFn}
     >
       <button slot="trigger">Integrate with Knit</button>
-    </KnitWeb> 
+    </KnitWeb>
   );
 }
 
