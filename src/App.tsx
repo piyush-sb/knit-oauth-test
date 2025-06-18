85356615933c177316f0c433697abb1790fc0d30
piyush-sb <piyush@getknit.dev>
2025-06-18 15:11:00 +0530

    fix : Lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-// import "./index.css";
-import Authorize from "./pages/Authorize";
-import MyKnit from "./pages/MyKnitWebClient.jsx";
-import MyKnitSandbox from "./pages/MyKnitWebClientSandbox.jsx";
+import "./index.css";
+const Authorize = React.lazy(() => import("./pages/Authorize"));
+const MyKnit = React.lazy(() => import("./pages/MyKnitWebClient.jsx"));
+const MyKnitSandbox = React.lazy(
+  () => import("./pages/MyKnitWebClientSandbox.jsx")
+);
 
-import AdminAuth from "./pages/AdminAuth";
+const AdminAuth = React.lazy(() => import("./pages/AdminAuth"));
 function App() {
   return (
     <div className="">
